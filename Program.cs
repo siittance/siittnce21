3d26af5a69a70a81008cab66b9064160a3c973b9
siittance <isip_yu.v.ivanova>
2023-09-14 13:10:18 +0300

    gg

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Text;
+
+Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+string text = Console.ReadLine();
+Console.WriteLine("Вы ввели:" + text);
+
+//Console.WriteLine("Введите ваш возраст:");
+//string txt = Console.ReadLine();
+//double num;
+//if (double.TryParse(txt, out num) == true)
+//{
+//    if (num < 0 || num > 100)
+//    {
+//        Console.WriteLine("Возраст не может быть таким");
+//    }
+//    else
+//    {
+//        if (num > 10)
+//        {
+//            num = num + 8;
+//            Console.WriteLine("Вам будет: " + num);
+//        }
+//        else
+//        {
+//            num = num + 23;
+//            Console.WriteLine("Вам будет: " + num);
+//        }
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Это не число");
+//}
